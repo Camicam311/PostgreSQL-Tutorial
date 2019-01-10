c9d9a5ddd1c0219d65e3648ff8dc4817dd1e0580
amandashu <shu.amanda25@gmail.com>
2019-01-09 19:19:53 -0800

    matched with readme

--- queries.sql
+++ queries.sql
@@ -1,4 +1,8 @@
 
+-- basic statement--
+SELECT *
+FROM students;
+
 -- which Weasley's are at Hogwarts right now? --
 SELECT *
 FROM students 
@@ -38,11 +42,46 @@ SELECT sex, COUNT(*)
 FROM students
 GROUP BY sex;
 
--- which house has more than 10 characters in it? -- 
+-- which house has more than 10 students in it? -- 
 SELECT house_id
 FROM students
 GROUP BY house_id
 HAVING COUNT(first_name) > 10;
+
+-- inner join syntax --
+SELECT *
+FROM table1
+INNER JOIN table2
+USING (id);
+
+-- left join syntax -- 
+SELECT *
+FROM table1
+LEFT JOIN table2
+USING (id);
+
+-- right join syntax -- 
+SELECT *
+FROM table1
+RIGHT JOIN table2
+USING (id);
+
+-- full join syntax -- 
+SELECT *
+FROM table1
+FULL JOIN table2
+USING (id);
+
+-- cross join syntax--
+SELECT *
+FROM table1
+CROSS JOIN table2;
+
+-- INNER JOIN professors and classes --
+SELECT *
+FROM professors
+INNER JOIN classes
+USING (prof_id);
 
 -- which teacher teaches Defense Against the Dark Arts? --
 SELECT prof_name
@@ -57,6 +96,12 @@ FROM students AS s
 INNER JOIN houses AS h
 ON s.house_id = h.house_id
 WHERE house_name = 'Gryffindor';
+
+-- students LEFT JOIN blood_line --
+SELECT *
+FROM students
+LEFT JOIN blood_line
+USING (student_id);
 
 -- are all Slytherins purebloods? -- 
 SELECT first_name, blood_type, house_name
@@ -76,8 +121,32 @@ RIGHT JOIN students
 USING (student_id)
 INNER JOIN houses
 USING (house_id)
-WHERE house_name = 'Slytherin' AND 
-	blood_type != 'pure'
+WHERE house_name = 'Slytherin' AND
+	(blood_type != 'pure'
+	OR blood_type ISNULL);
+
+-- how many teachers are classified as new, some experience, and much experience --
+SELECT COUNT(*),
+	CASE WHEN num_years>0 AND num_years<=5 THEN 'new'
+	WHEN num_years>5 AND num_years<=15 THEN 'some experience'
+	ELSE 'much experience' END
+	AS experience_level
+FROM professors
+GROUP BY experience_level;
+
+-- which professors taught for longer than the average? --
+SELECT prof_name
+FROM professors
+WHERE num_years >
+	(SELECT AVG(num_years)
+	FROM professors);
+
+-- union dums_array with inquisitorial_squad -- 
+SELECT *
+FROM dums_army
+UNION
+SELECT *
+FROM inquisitorial_squad);
 
 -- who is not in Dumbledore's Army nor in the Inquisitorial Squad? --
 SELECT first_name, last_name
@@ -102,35 +171,6 @@ INNER JOIN enrollment
 USING (class_id)
 WHERE student_id = 3
 
--- which classes are Hermione taking but not Harry? -- 
-SELECT class_id, class_name
-FROM classes
-INNER JOIN enrollment
-USING (class_id)
-WHERE student_id = 3
-EXCEPT
-SELECT class_id, class_name
-FROM classes
-INNER JOIN enrollment
-USING (class_id)
-WHERE student_id = 1
-
--- which professors taught for longer than the average? --
-SELECT prof_name 
-FROM professors
-WHERE num_years >
-	(SELECT AVG(num_years)
-	FROM professors);
-
--- how many teachers are classified as new, some experience, and much experience --
-SELECT COUNT(*),
-	CASE WHEN num_years>0 AND num_years<=5 THEN 'new'
-	WHEN num_years>5 AND num_years<=15 THEN 'some experience'
-	ELSE 'much experience' END
-	AS experience_level
-FROM professors
-GROUP BY experience_level;
-
 -- how many students are in each house? --
 SELECT house_name,
 	(SELECT COUNT(*)
@@ -145,6 +185,3 @@ FROM classes,
 	FROM enrollment
 	GROUP BY class_id) AS subquery
 WHERE classes.class_id = subquery.class_id;
-
-
-	
